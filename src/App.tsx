b49f6dcb6e6cd394014f8dda4d9157e9bbc1ecee
SeongJun-Hwang <hsjbusniss@gmail.com>
2022-01-24 00:41:58 +0900

    useParams

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,7 @@
-import React, { useState } from 'react';
-import styled from "styled-components"
+import Router from './Router';
 
-const Wrapper = styled.div `
-  display: flex;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const Text = styled.h1 `
-  align-items: center;
-  color: ${(props) => props.theme.textColor};
-  font-family: sans-serif;
-  font-size: 30px;
-  margin-right: 50px;
-`;
-
-const Cent = styled.div `
-  justify-content: center;
-  align-items: center;
-`;
 function App() {
-  const [value, Setvalue] = useState<string>("")
-  const [greeting, Setgreeting] = useState<string>("HELLO")
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    Setvalue(value)
-    // console.log(event.currentTarget.value)
-  }
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    let greeting = 'Hello ' + value
-    Setgreeting(greeting)
-    console.log('Hello', value)
-  }
-
-  return (
-    <Wrapper>
-      <Cent>
-        <Text>{greeting}</Text>
-        <form onSubmit={onSubmit}>
-          <input value={value} onChange={onChange} type="text" placeholder='username' />
-          <button>Log In</button>
-        </form>
-      </Cent>
-    </Wrapper>
-  );
+  return <Router />
 }
 
 export default App;
